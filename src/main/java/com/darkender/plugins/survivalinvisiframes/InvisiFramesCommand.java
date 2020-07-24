f0573769ccbebac7fb1a59be8f492d549af794b4
Techdoodle <26680599+TheTechdoodle@users.noreply.github.com>
2020-07-23 22:03:23 -0700

    set up permissions and structure for subcommands

--- src/main/java/com/darkender/plugins/survivalinvisiframes/InvisiFramesCommand.java
+++ src/main/java/com/darkender/plugins/survivalinvisiframes/InvisiFramesCommand.java
@@ -6,7 +6,9 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
+import org.bukkit.util.StringUtil;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -15,21 +17,80 @@ public class InvisiFramesCommand implements CommandExecutor, TabCompleter
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
     {
-        if(!(sender instanceof Player))
+        if(args.length == 0 || args[0].equalsIgnoreCase("get"))
         {
-            sender.sendMessage(ChatColor.RED + "Sorry, you must be a player to use this command!");
+            giveItem(sender);
             return true;
         }
-        
-        Player player = (Player) sender;
-        player.getInventory().addItem(SurvivalInvisiframes.generateInvisibleItemFrame());
-        player.sendMessage(ChatColor.GREEN + "Added an invisible item frame to your inventory");
-        return true;
+        else if(args[0].equalsIgnoreCase("reload"))
+        {
+            if(!sender.hasPermission("survivalinvisiframes.reload"))
+            {
+                sendNoPermissionMessage(sender);
+                return true;
+            }
+            //TODO reload logic
+            return true;
+        }
+        else if(args[0].equalsIgnoreCase("force-recheck"))
+        {
+            if(!sender.hasPermission("survivalinvisiframes.forcerecheck"))
+            {
+                sendNoPermissionMessage(sender);
+                return true;
+            }
+            //TODO force recheck logic
+            return true;
+        }
+        return false;
     }
     
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args)
     {
-        return Collections.emptyList();
+        if(args.length != 1)
+        {
+            return Collections.emptyList();
+        }
+        List<String> options = new ArrayList<>();
+        if(sender.hasPermission("survivalinvisiframes.get"))
+        {
+            options.add("get");
+        }
+        if(sender.hasPermission("survivalinvisiframes.reload"))
+        {
+            options.add("reload");
+        }
+        if(sender.hasPermission("survivalinvisiframes.forcerecheck"))
+        {
+            options.add("force-recheck");
+        }
+        List<String> completions = new ArrayList<>();
+        StringUtil.copyPartialMatches(args[0], options, completions);
+        Collections.sort(completions);
+        return completions;
+    }
+    
+    private void sendNoPermissionMessage(CommandSender sender)
+    {
+        sender.sendMessage(ChatColor.RED + "Sorry, you don't have permission to run this command");
+    }
+    
+    private void giveItem(CommandSender sender)
+    {
+        if(!sender.hasPermission("survivalinvisiframes.get"))
+        {
+            sendNoPermissionMessage(sender);
+            return;
+        }
+        if(!(sender instanceof Player))
+        {
+            sender.sendMessage(ChatColor.RED + "Sorry, you must be a player to use this command!");
+            return;
+        }
+        
+        Player player = (Player) sender;
+        player.getInventory().addItem(SurvivalInvisiframes.generateInvisibleItemFrame());
+        player.sendMessage(ChatColor.GREEN + "Added an invisible item frame to your inventory");
     }
 }
